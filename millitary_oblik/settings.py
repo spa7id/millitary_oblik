6f882fbb37678db258b7104b66ecac9b92d0a11a
spa7id <ivovan4ik@gmail.com>
2026-03-06 21:41:04 +0200

    refactor setting.py

--- millitary_oblik/settings.py
+++ millitary_oblik/settings.py
@@ -3,24 +3,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = "django-insecure-pb@fex2mq9=@w+ek^)9h#qan+$kyoxe6o(7w8q(8ih$i%tuggo"
-
 SECRET_KEY = os.getenv("SECRET_KEY")
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
 DEBUG = os.getenv("DEBUG", "False") == "True"
-
-#ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
