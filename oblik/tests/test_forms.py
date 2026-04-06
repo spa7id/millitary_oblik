17dae3ca0c83dc0ba5724cd1631ccd968ac90be9
spa7id <ivovan4ik@gmail.com>
2026-04-06 23:37:15 +0300

    add some tests for forms.py

--- oblik/tests/test_forms.py
+++ oblik/tests/test_forms.py
@@ -68,3 +68,80 @@ class ServiceMemberFormTest(TestCase):
     def test_user_field_excluded(self):
         form = ServiceMemberForm()
         self.assertNotIn("user", form.fields)
+
+class UnitFormTest(TestCase):
+    def setUp(self):
+        self.valid_data = {
+            "name": "1 взвод",
+            "unit_type": "взвод",
+            "parent": "",
+        }
+
+    def test_valid_form(self):
+        form = UnitForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_parent_optional(self):
+        data = self.valid_data.copy()
+        data["parent"] = ""
+        form = UnitForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_with_parent_unit(self):
+        parent = Unit.objects.create(name="3 рота", unit_type="рота")
+        data = self.valid_data.copy()
+        data["parent"] = parent.pk
+        form = UnitForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_required_fields(self):
+        form = UnitForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertIn("name", form.errors)
+        self.assertIn("unit_type", form.errors)
+
+    def test_all_fields_have_form_control_class(self):
+        form = UnitForm()
+        for field_name, field in form.fields.items():
+            self.assertEqual(field.widget.attrs.get("class"), "form-control")
+
+    def test_name_placeholder_set(self):
+        form = UnitForm()
+        self.assertIn("placeholder", form.fields["name"].widget.attrs)
+
+
+class UserRegisterFormTest(TestCase):
+    def setUp(self):
+        self.valid_data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password1": "StrongPass123!",
+            "password2": "StrongPass123!",
+        }
+
+    def test_valid_form(self):
+        form = UserRegisterForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_passwords_dont_match(self):
+        data = self.valid_data.copy()
+        data["password2"] = "DifferentPass123!"
+        form = UserRegisterForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_email(self):
+        data = self.valid_data.copy()
+        data["email"] = "not-an-email"
+        form = UserRegisterForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors)
+
+    def test_email_field_present(self):
+        form = UserRegisterForm()
+        self.assertIn("email", form.fields)
+
+    def test_required_fields_empty(self):
+        form = UserRegisterForm(data={})
+        self.assertFalse(form.is_valid())
+        for field in ["username", "password1", "password2"]:
+            self.assertIn(field, form.errors)
